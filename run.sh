d23213639b400c8840391fee9a61b93de3f336e8
kylebarts <kpibartido@gmail.com>
2018-04-21 22:08:06 +0800

    Changed Filenames

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-g++ Project2MedinaBartido.cpp Process.h Process.cpp -o output -std=c++11
-./output input.txt
+g++ CE155Project2_Bartido_Medina_code.cpp Process.h Process.cpp -o output -std=c++11
+./output CE155Project2_Bartido_Medina_testfile.txt
